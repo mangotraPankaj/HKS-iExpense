34d0730dfa330f717546d611bc9c6885cfa9e738
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2021-07-08 17:53:57 +0530

    Demo code to show delete feature on ForEach in List

--- HKS-iExpense/ContentView.swift
+++ HKS-iExpense/ContentView.swift
@@ -7,10 +7,34 @@
 
 import SwiftUI
 
+class User: ObservableObject {
+    @Published var firstName = "Bilbo"
+    @Published var lastName = "Baggins"
+}
+
 struct ContentView: View {
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                List {
+                    ForEach(numbers, id: \.self) {
+                        Text("\($0)")
+                    }
+                    .onDelete(perform: removeRows(at:))
+                }
+                Button("Add Number") {
+                    self.numbers.append(self.currentNumber)
+                    self.currentNumber += 1
+                }
+            }.navigationBarItems(leading: EditButton())
+        }
+    }
+
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
     }
 }
 
