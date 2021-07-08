52a8ddb2f7ceb37ef1f3b410a9c7419ded67fc7e
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2021-07-08 17:22:15 +0530

    Initial Commit

---
+++ HKS-iExpense/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HKS-iExpense
+//
+//  Created by Pankaj Mangotra on 08/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HKS-iExpense/HKS_iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  HKS_iExpenseApp.swift
+//  HKS-iExpense
+//
+//  Created by Pankaj Mangotra on 08/07/21.
+//
+
+import SwiftUI
+
+@main
+struct HKS_iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
