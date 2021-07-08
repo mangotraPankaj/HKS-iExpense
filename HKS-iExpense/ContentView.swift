bf662a31dacdd7954d6671df95440ebd11f90d4d
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2021-07-08 19:35:58 +0530

    Demo code for codable

--- HKS-iExpense/ContentView.swift
+++ HKS-iExpense/ContentView.swift
@@ -7,17 +7,21 @@
 
 import SwiftUI
 
-class User: ObservableObject {
-    @Published var firstName = "Bilbo"
-    @Published var lastName = "Baggins"
+struct User: Codable {
+    var firstName : String
+    var lastName: String
 }
 
 struct ContentView: View {
-    @State private var currentNumber = UserDefaults.standard.integer(forKey: "Tap")
-
+    
+    @State private var user = User(firstName: "Taylor", lastName: "Swift")
     var body: some View {
-        Button("Tap Count: \(currentNumber)") {
-            self.currentNumber += 1
+        Button("Save User") {
+            let encoder = JSONEncoder()
+            
+            if let data = try? encoder.encode(self.user) {
+                UserDefaults.standard.set(data, forKey: "UserData")
+            }
         }
     }
 }
