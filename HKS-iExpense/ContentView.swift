6f79381d429b23c5113ed532fc2a2a6baf22de41
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2021-07-15 12:39:18 +0530

    Encoding and decoding the expense items using JSON encoder.

--- HKS-iExpense/ContentView.swift
+++ HKS-iExpense/ContentView.swift
@@ -7,15 +7,34 @@
 
 import SwiftUI
 
-struct ExpenseItem: Identifiable {
+struct ExpenseItem: Identifiable, Codable {
     let name: String
     let type: String
     let amount: Int
-    let id = UUID()
+    var id = UUID()
 }
 
 class Expenses: ObservableObject {
-    @Published var items = [ExpenseItem]()
+    @Published var items = [ExpenseItem]() {
+        didSet {
+            let encoder = JSONEncoder()
+            if let encoded = try? encoder.encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+
+    init() {
+        if let items = UserDefaults.standard.data(forKey: "Items") {
+            let decoder = JSONDecoder()
+            if let decoded = try? decoder.decode([ExpenseItem].self, from: items) {
+                self.items = decoded
+                return
+            }
+        } else {
+            self.items = []
+        }
+    }
 }
 
 struct ContentView: View {
@@ -34,7 +53,7 @@ struct ContentView: View {
             .sheet(isPresented: $showAddExpense) {
                 AddView(expenses: self.expenses)
             }
-            
+
             .navigationBarTitle("iExpense")
             .navigationBarItems(trailing: Button(action: {
                 self.showAddExpense = true
